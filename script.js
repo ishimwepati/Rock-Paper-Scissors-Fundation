1d2242167cdbe12e20e602e62b31916d541894d4
ishimwepati <wazacode@gmail.com>
2023-04-04 11:42:22 +0300

    Function created in script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Welcome to my Foundation")
+console.log("Rock Scissor Paper Game by WazaCode")
+
+const Player = '';
+const Computer = '';
+const winner = '';
+
+function playRound(ROCK, PAPER, SCISSOR){
+
+}
+
+function getComputerChoice (Rock, Paper, Scissors){
+
+    return 
+}
